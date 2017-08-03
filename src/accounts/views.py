ebab3abf51590871123eb57af6548c5e00ff8a58
saadmk11 <saad.mk11@hotmail.com>
2017-08-04 01:07:48 +0600

    login register bug fixed & requirements.txt added

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -12,37 +12,43 @@ from .models import User
 
 # Create your views here.
 def login_view(request): # users will login with their Email & Password
-    title = "Login"
-    form = UserLoginForm(request.POST or None)
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
-        # authenticates Email & Password
-        user = authenticate(email=email, password=password) 
-        login(request, user)
+    if request.user.is_authenticated:
         return redirect("home")
-    context = {"form":form,
-               "title":title
-    }
+    else:
+        title = "Login"
+        form = UserLoginForm(request.POST or None)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            # authenticates Email & Password
+            user = authenticate(email=email, password=password) 
+            login(request, user)
+            return redirect("home")
+        context = {"form":form,
+                   "title":title
+        }
 
-    return render(request, "accounts/form.html", context)
+        return render(request, "accounts/form.html", context)
 
 
 def register_view(request): # Creates a New Account & login New users
-    title = "Register"
-    form = UserRegistrationForm(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        password = form.cleaned_data.get("password1")
-        user.set_password(password)
-        user.save()
-        new_user = authenticate(email=user.email, password=password)
-        login(request, new_user)
+    if request.user.is_authenticated:
         return redirect("home")
+    else:
+        title = "Register"
+        form = UserRegistrationForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data.get("password1")
+            user.set_password(password)
+            user.save()
+            new_user = authenticate(email=user.email, password=password)
+            login(request, new_user)
+            return redirect("home")
 
-    context = {"title":title, "form":form}
+        context = {"title":title, "form":form}
 
-    return render(request, "accounts/form.html", context)
+        return render(request, "accounts/form.html", context)
 
 
 def logout_view(request): # logs out the logged in users
